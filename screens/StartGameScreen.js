30945b7df46a7ca23ec270e7ddb2c41d82345dcc
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-23 08:14:59 -0600

    clean user input and control the soft keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,33 +1,51 @@
-import React from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import {
+    Text,
+    View,
+    StyleSheet,
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native'
 import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
 
 const StartGameScreen = (props) => {
+    const [enteredValue, setEnteredValue] = useState('')
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <Input
-                    style={styles.input}
-                    blurOnSubmit
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    keyboardType='number-pad'
-                    maxLength='2'
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss()
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input
+                        style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType='number-pad'
+                        maxLength='2'
+                        onChangeText={numberInputHandler}
+                        values={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+                        </View>
+                        <View>
+                            <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
+                        </View>
                     </View>
-                    <View>
-                        <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
-                    </View>
-                </View>
-            </Card>
-        </View >
+                </Card>
+            </View >
+        </TouchableWithoutFeedback>
     )
 }
 const styles = StyleSheet.create({
