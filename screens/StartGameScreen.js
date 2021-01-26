1754ede66133c9b780feea7e9d3dabf4cb1aa243
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-26 08:32:22 -0600

    implement custom wrapper components and global styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,6 +13,8 @@ import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
 import Colors from '../constants/colors';
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -63,9 +65,9 @@ const StartGameScreen = props => {
             }}
         >
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input
                         style={styles.input}
                         blurOnSubmit
@@ -131,6 +133,9 @@ const styles = StyleSheet.create({
     summaryContainer: {
         marginTop: 20,
         alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'open-sans'
     }
 });
 
